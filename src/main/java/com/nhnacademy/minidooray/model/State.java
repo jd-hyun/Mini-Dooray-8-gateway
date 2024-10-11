9982183acc0d8ffb87a6d0562cad2b3e9bb79491
FlowerfulFort <skagyghk@naver.com>
2024-10-11 22:58:17 +0900

    [UPDATE] Project view now shows table

--- src/main/java/com/nhnacademy/minidooray/model/State.java
+++ src/main/java/com/nhnacademy/minidooray/model/State.java
@@ -1,9 +1,14 @@
 package com.nhnacademy.minidooray.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
 public enum State {
-    ACTIVE, SLEEP, TERMINATED;
+    ACTIVE("활성"), SLEEP("휴면"), TERMINATED("종료");
+    private final String view;
 
     public String toCssClass() {
         return this.name().toLowerCase();
