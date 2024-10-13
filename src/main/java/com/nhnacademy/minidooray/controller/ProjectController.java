e992a4696c86892dba28b201fcef1e94c3f8af5d
FlowerfulFort <skagyghk@naver.com>
2024-10-13 15:35:39 +0900

    [NEW] Addition Members feature

--- src/main/java/com/nhnacademy/minidooray/controller/ProjectController.java
+++ src/main/java/com/nhnacademy/minidooray/controller/ProjectController.java
@@ -7,20 +7,34 @@ import com.nhnacademy.minidooray.model.rest.project.ProjectUpdateRequest;
 import com.nhnacademy.minidooray.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @RequestMapping("/project")
 @Slf4j
 @Controller
 public class ProjectController {
     private final ProjectService projectService;
+
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @Qualifier("accountRequestBuilder")
+    @Autowired
+    private ObjectProvider<UriComponentsBuilder> builderProvider;
 
     // TODO: Project Service로 리팩토링하기
     @GetMapping
@@ -39,12 +53,16 @@ public class ProjectController {
     @GetMapping("/{projectId}")
     public String getProjectPage(@AuthenticationPrincipal UserDetails user, @PathVariable long projectId, Model model) {
         ProjectDetailDto detail = projectService.getProjectDetailById(projectId);
+
+        UriComponents components = builderProvider.getIfAvailable().path("/accounts?like=").build();
+
 //        String username = user.getUsername();
 //        boolean isMember = detail.getMembers().stream().anyMatch(member -> member.getId().equals(username));
 //
 //        if (detail.getAuthorId().equals(username) || isMember) {
         if (true) {
             model.addAttribute("project", detail);
+            model.addAttribute("memberRequestPath", components.toUriString());
 //            model.addAllAttributes(detail.toModelMap());
             return "project/project_detail";
         } else {
