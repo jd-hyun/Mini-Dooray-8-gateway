3a83320afa4d216ca622a12871aca24838b52740
jd-hyun <wjdehdgus3540@naver.com>
2024-10-15 02:09:10 +0900

    과제 제출

--- src/main/java/com/nhnacademy/minidooray/MinidoorayGatewayApplication.java
+++ src/main/java/com/nhnacademy/minidooray/MinidoorayGatewayApplication.java
@@ -2,8 +2,10 @@ package com.nhnacademy.minidooray;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MinidoorayGatewayApplication {
 
     public static void main(String[] args) {
